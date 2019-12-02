02765fb8a9697f22dc54a4b93fff85a33c8a4835
Richard Oosterlaken <richard.oosterlaken@gmail.com>
2019-12-02 22:25:54 +0100

    Add day 2 solutions.

--- stars/__init__.py
+++ stars/__init__.py
@@ -1,4 +1,5 @@
 from stars.star1 import run as run_star1
 from stars.star2 import run as run_star2
+from stars.star3and4 import run_star3, run_star4
 
-run_functions = [run_star1, run_star2]
+run_functions = [run_star1, run_star2, run_star3, run_star4]
---
+++ stars/star3and4.py
@@ -0,0 +1,53 @@
+class IntcodeComputer:
+    def __init__(self, input_string):
+        self.initial_state = [int(value) for value in input_string.split(",")]
+        self.memory = self.initial_state
+        self.instruction_pointer = 0
+
+    def add(self, first_index, second_index, storage_index):
+        self.memory[storage_index] = (
+            self.memory[first_index] + self.memory[second_index]
+        )
+
+    def multiply(self, first_index, second_index, storage_index):
+        self.memory[storage_index] = (
+            self.memory[first_index] * self.memory[second_index]
+        )
+
+    def run(self):
+        self.instruction_pointer = 0
+        running = True
+        while running:
+            start_address = self.instruction_pointer
+            instruction = self.memory[start_address : start_address + 4]
+            opcode = instruction[0]
+
+            if opcode == 1:
+                self.add(*instruction[1:])
+            elif opcode == 2:
+                self.multiply(*instruction[1:])
+            elif opcode == 99:
+                running = False
+                break
+
+            self.instruction_pointer += 4
+
+
+def run_star3():
+    with open("input/star3.input") as input_file:
+        input_text = input_file.read().strip()
+    ic = IntcodeComputer(input_text)
+    ic.memory[1] = 12
+    ic.memory[2] = 2
+    ic.run()
+    print(f"[Intcode Computer] Value at position 0: {ic.memory[0]}")
+
+
+def run_star4():
+    with open("input/star3.input") as input_file:
+        input_text = input_file.read().strip()
+    ic = IntcodeComputer(input_text)
+    ic.memory[1] = 76
+    ic.memory[2] = 3
+    ic.run()
+    print(f"[Intcode Computer] Value at position 0: {ic.memory[0]}")
