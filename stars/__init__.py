ddd6858ae3f742543ab33b427abf3614dffd06cc
Richard Oosterlaken <richard.oosterlaken@gmail.com>
2019-12-05 17:41:55 +0100

    Add stars 9 and 10.

--- stars/__init__.py
+++ stars/__init__.py
@@ -3,6 +3,7 @@ from stars.star2 import run as run_star2
 from stars.star3and4 import run_star3, run_star4
 from stars.star5and6 import run_star5, run_star6
 from stars.star7and8 import run_star7, run_star8
+from stars.star9and10 import run_star9, run_star10
 
 run_functions = [
     run_star1,
@@ -13,4 +14,6 @@ run_functions = [
     run_star6,
     run_star7,
     run_star8,
+    run_star9,
+    run_star10,
 ]
